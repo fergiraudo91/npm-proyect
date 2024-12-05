d88747f3099894d100eddd4c06b024165d7f623a
Fernando Giraudo <fergiraudo91@gmail.com>
2024-12-05 20:17:23 -0300

    functionality added

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-function saludar(nombre) {
-    return`Hola ${nombre}`;
+export function suma(num1, num2){
+    return num1 + num2;
+}
+
+export function resta(num1, num2){
+    return num1 - num2;
+}
+
+export function multiplicacion(num1, num2){
+    return num1 * num2;
+}
+
+export function division(num1, num2){
+    return num1 / num2;
 }
